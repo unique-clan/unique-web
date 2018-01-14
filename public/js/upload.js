f59f3bff40479a8b28af73b695fdd428920a7673
Ryozuki <clud6565@gmail.com>
2018-01-14 10:07:09 +0000

    Revert "gitignore working"
    This reverts commit 89f165ab0f196490843ea3cb69731518eecd39ec

---
+++ public/js/upload.js
@@ -0,0 +1,9 @@
+var file = document.getElementById("file");
+file.onchange = function(){
+    if(file.files.length > 0)
+    {
+
+      document.getElementById('filename').innerHTML = 					file.files[0].name;
+
+    }
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Unique Clan Website
+Copyright (c) 2018 Ryozuki, Tezcan, timakro
+
+You can check the todo under the TODO file.
