b6f204d528794d3d0364bc9b8f0411ecadabaff7
Ryozuki <clud6565@gmail.com>
2018-01-14 12:08:08 +0100

    added mongoose, plugins and models

---
+++ mongodb/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+// Models
+var user = require('./models/user')
+// Plugins
+var lastmod = require('./plugins/lastmod')
+
+module.exports = exports = function load () {
+  user.plugin(lastmod)
+  mongoose.model('User', user)
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var debug = require('debug')('uniqueweb:router')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
