9d79e5f51e97b09d0b9572ad157c8195e1984fdd
Ryozuki <edgar@ryobyte.com>
2018-02-05 23:01:50 +0100

    remove unused

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ var express = require('express')
 var router = express.Router()
 var debug = require('debug')('uniqueweb:auth')
 var mongoose = require('mongoose')
-var UserSchema = require('../database/models/user')
 var middleware = require('./middleware')
 
 var Recaptcha = require('express-recaptcha')
