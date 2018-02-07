e390059f76e730b9e60d621b54038643156c1c64
Tim Schumacher <tim@timakro.de>
2018-02-07 11:22:13 +0100

    Design comming soon ...

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var router = express.Router()
 var debug = require('debug')('uniqueweb:router')
-var serverstatus = require('./serverstatus')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,22 +14,6 @@ router.get('/member', function (req, res, next) {
   res.render('member', {
     title: 'Members | Unique',
     user: req.session.authed ? req.session.user : null
-  })
-})
-
-router.get('/serverstatus', function (req, res, next) {
-  res.redirect('/serverstatus/' + Object.keys(serverstatus.locations)[0])
-})
-
-router.get('/serverstatus/:location', function (req, res, next) {
-  let location = req.params.location
-  if (!(location in serverstatus.locations))
-    res.sendStatus(404)
-  res.render('serverstatus', {
-    title: location + ' Server Status | Unique',
-    user: req.session.authed ? req.session.user : null,
-    locations: serverstatus.locations,
-    location: location
   })
 })
 
@@ -56,3 +39,4 @@ router.get('/ranks', function (req, res, next) {
 })
 
 module.exports = router
+require('./serverstatus')
