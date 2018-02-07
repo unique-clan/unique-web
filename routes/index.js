b5c7f5f764d0be1e9010a8b41db6dc0518d8a444
Tezcan <umit-tezcan@hotmail.de>
2018-02-07 17:18:42 +0100

    add tournament

--- routes/index.js
+++ routes/index.js
@@ -38,5 +38,12 @@ router.get('/ranks', function (req, res, next) {
   })
 })
 
+router.get('/tournaments', function (req, res, next) {
+  res.render('tournaments', {
+    title: 'Tournaments | Unique',
+    user: req.session.authed ? req.session.user : null
+  })
+})
+
 module.exports = router
 require('./serverstatus')
