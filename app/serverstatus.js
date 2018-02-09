059f847a302030d5016d8955902afb46fa188a17
Tim Schumacher <tim@timakro.de>
2018-02-09 17:43:40 +0100

    Dummy data

--- app/serverstatus.js
+++ app/serverstatus.js
@@ -59,17 +59,131 @@ class ServerStatus {
     // This is for now a placeholder, all sv are same
     for (var j in server.servers) {
       let gameServer = server.servers[j]
-
-      gameServer.reachable = true
-      gameServer.map = 'run_blabla'
-      gameServer.gametype = 'Race'
-      gameServer.numclients = 3
-      gameServer.maxclients = 32
-      gameServer.players = [
-        { name: 'timakro', clan: 'υηιqυє', flag: 'default', score: 19 },
-        { name: 'Tezcan', clan: 'Yo xD', flag: this.twFlags[203], score: 19 },
-        { name: 'Ryozuki', clan: 'hallo', flag: this.twFlags[906], score: 19 }
-      ]
+      if (gameServer.name === 'OpenFNG') {
+        gameServer.reachable = true
+        gameServer.map = 'OpenFNG5beat'
+        gameServer.numclients = 3
+        gameServer.maxclients = 16
+        gameServer.players = [
+          { name: 'timakro', clan: 'υηιqυє', flag: 'default', score: 19 },
+          { name: 'Tezcan', clan: 'Yo xD', flag: this.twFlags[203], score: 30 },
+          { name: 'Ryozuki', clan: 'hallo', flag: this.twFlags[906], score: 19 }
+        ]
+      }
+      if (gameServer.name === 'TeeSmash') {
+        gameServer.reachable = true
+        gameServer.map = 'smashDex'
+        gameServer.numclients = 2
+        gameServer.maxclients = 16
+        gameServer.players = [
+          { name: 'Tezcan', clan: 'Yo xD', flag: this.twFlags[203], score: 1 },
+          { name: 'Ryozuki', clan: 'hallo', flag: this.twFlags[906], score: 19 }
+        ]
+      }
+      if (gameServer.name === 'Fly') {
+        gameServer.reachable = true
+        gameServer.map = 'run_fast'
+        gameServer.numclients = 5
+        gameServer.maxclients = 16
+        gameServer.players = [
+          { name: 'Tezcan', clan: 'Yo xD', flag: this.twFlags[203], score: 19 },
+          { name: 'Ryozuki', clan: 'hallo', flag: this.twFlags[96], score: 3 },
+          { name: 'dcgu', clan: '', flag: this.twFlags[8], score: 19 },
+          { name: 'Freezestyler', clan: 'roguelike', flag: this.twFlags[533], score: 7 },
+          { name: 'Men Of Steel', clan: 'Awesome', flag: this.twFlags[166], score: 19 }
+        ]
+      }
+      if (gameServer.name === 'Football') {
+        gameServer.reachable = true
+        gameServer.map = 'teefoot1'
+        gameServer.numclients = 0
+        gameServer.maxclients = 16
+      }
+      if (gameServer.name === 'Race 1') {
+        gameServer.reachable = true
+        gameServer.map = 'run_orange'
+        gameServer.numclients = 8
+        gameServer.maxclients = 64
+        gameServer.players = [
+          { name: 'AAAAAAAAAAAAAAA', clan: 'AAAAAAAAAAAAAAA', flag: 'default', score: '23:23.333' },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 }
+        ]
+      }
+      if (gameServer.name === 'Race 2') {
+        gameServer.reachable = true
+        gameServer.map = 'run_greeen'
+        gameServer.numclients = 5
+        gameServer.maxclients = 64
+        gameServer.players = [
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 }
+        ]
+      }
+      if (gameServer.name === 'Instagib') {
+        gameServer.reachable = true
+        gameServer.map = 'dark_laser'
+        gameServer.numclients = 15
+        gameServer.maxclients = 16
+        gameServer.players = [
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 }
+        ]
+      }
+      if (gameServer.name === 'SoloFNG') {
+        gameServer.reachable = true
+        gameServer.map = 'openfng2'
+        gameServer.numclients = 0
+        gameServer.maxclients = 16
+      }
+      if (gameServer.name === 'BoomFNG 1') {
+        gameServer.reachable = true
+        gameServer.map = 'openfng5'
+        gameServer.numclients = 7
+        gameServer.maxclients = 16
+        gameServer.players = [
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 },
+          { name: 'nameless tee', clan: '', flag: 'default', score: 0 }
+        ]
+      }
+      if (gameServer.name === 'BoomFNG 2') {
+        gameServer.map = 'dm1'
+        gameServer.reachable = true
+        gameServer.numclients = 0
+        gameServer.maxclients = 16
+      }
+      if (gameServer.name === 'Bomb') {
+        gameServer.map = 'BombGarden'
+        gameServer.reachable = true
+        gameServer.numclients = 0
+        gameServer.maxclients = 16
+      }
     }
   }
 }
