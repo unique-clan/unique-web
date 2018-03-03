23653a95b3121d7612971773feb9c96072f3b376
Tim Schumacher <tim@timakro.de>
2018-03-03 23:23:50 +0100

    Ready for production?

---
+++ public/js/mapupload.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  var form = new BulmaFormHandler('#form-mapupload', '/mapupload', 'POST', true)
+  form.onResponse((status, res) => {
+    switch (status) {
+      case 400:
+      case 422:
+      {
+        form.handleErrors(res.responseJSON.errors)
+        break
+      }
+      case 201:
+      {
+        form.turnAllGreen()
+        form.find('p#response-message').html(res.responseJSON.msg)
+        break
+      }
+    }
+  })
+})
+
+var file = $('#file')
+function updateFilename() {
+  let filename = file.val().split('\\').pop()
+  if (filename) {
+    $('#filename').text(filename)
+    $('#filename').removeClass('has-text-grey-light')
+  }
+}
+file.change(updateFilename)
+$(document).ready(updateFilename)
