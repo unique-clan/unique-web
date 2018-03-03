f83cf59508628619f7b6d1b7964eadeab954f67e
Tim Schumacher <tim@timakro.de>
2018-03-03 23:23:50 +0100

    Ready for production?

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express')
 var router = express.Router()
 var debug = require('debug')('uniqueweb:router')
+var fs = require('fs');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 const ServerStatus = require('../app/serverstatus')
 
 var serverStatus = new ServerStatus(process.env.SERVERS_LOCATION || 'servers.json')
 serverStatus.startUpdating()
+
+function getFilename (folder, name) {
+  let path = folder + '/' + name.replace(/[^\w\s\.]/g, '')
+  if (fs.existsSync(path)) {
+    let n = 2
+    while (true) {
+      if (!fs.existsSync(path+n))
+        return path+n
+      n += 1
+    }
+  } else {
+    return path
+  }
+}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -46,11 +63,71 @@ router.get('/apply', function (req, res, next) {
   })
 })
 
+router.post('/apply', function (req, res, next) {
+  let errors = {}
+  let error = false
+  let application = {}
+  const fields = ['twName', 'country', 'gameModes', 'gender', 'presentation']
+  for (var i in fields) {
+    let f = fields[i]
+    let value = req.body[f]
+    if (f === 'gameModes') {
+      value = value.split(',').filter(g => g.length)
+    }
+    if (!value || !value.length) {
+      errors[f] = { message: 'Field is required.' }
+      error = true;
+    }
+    application[f] = value
+  }
+  if (error) {
+    res.status(422).json({ errors: errors })
+    return
+  }
+  let path = getFilename('admintmp/applications', req.body['twName'])
+  fs.writeFileSync(path, JSON.stringify(application, null, 2))
+  res.status(201).json({ msg: 'Application sent.' })
+})
+
 router.get('/mapupload', function (req, res, next) {
   res.render('mapupload', {
     title: 'Map Upload | Unique',
     user: req.session.authed ? req.session.user : null
   })
+})
+
+router.post('/mapupload', upload.single('mapFile'), function (req, res, next) {
+  let errors = {}
+  let error = false
+  let mappers = []
+  if (req.body.mappers) {
+    mappers = req.body.mappers.split(',').filter(g => g.length)
+  }
+  if (!mappers.length) {
+    errors.mappers = { message: 'Field is required.' }
+    error = true
+  }
+  if (!req.file) {
+    errors.mapFile = { message: 'File is required.' }
+    error = true
+  } else if (!req.file.originalname.endsWith('.map')) {
+    errors.mapFile = { message: 'Filename must end on .map' }
+    error = true
+  }
+  if (error) {
+    if (req.file) {
+      fs.unlinkSync(req.file.path)
+    }
+    res.status(422).json({ errors: errors })
+    return
+  }
+  let name = req.file.originalname.substring(0, req.file.originalname.length - 4)
+  let path = getFilename('admintmp/map-submissions', name)
+  fs.mkdirSync(path)
+  fs.renameSync(req.file.path, getFilename(path, req.file.originalname))
+  let info = { filename: req.file.originalname, mappers: mappers }
+  fs.writeFileSync(path+'/info', JSON.stringify(info, null, 2))
+  res.status(201).json({ msg: 'Map submission sent.' })
 })
 
 router.get('/tournaments', function (req, res, next) {
