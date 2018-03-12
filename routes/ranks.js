d776154eb44af62896962a803c9b95cb91517f48
Tim Schumacher <tim@timakro.de>
2018-03-12 19:56:19 +0100

    Ranks design 1

--- routes/ranks.js
+++ routes/ranks.js
@@ -26,8 +26,6 @@ function formatTime (time) {
 router.get('/', async function (req, res, next) {
   const connection = await mysql.createConnection(mysqlOptions)
 
-  const [mapCount] = await connection.execute('SELECT count(*) as numMaps FROM race_maps')
-
   const [mapRecords] = await connection.execute('SELECT @pos := @pos + 1 AS v1, @rank := IF(@prev = recordsCount, @rank, @pos) AS rank, @prev := recordsCount AS v2, Name, recordsCount FROM (SELECT Name, COUNT(*) as recordsCount FROM (SELECT Name, t1.Map as Map FROM (SELECT Map, Name, ROUND(Time, 3) AS playerTime FROM race_race) t1 INNER JOIN (SELECT Map, ROUND(MIN(Time), 3) AS bestTime FROM race_race GROUP BY Map) t2 ON t1.Map = t2.Map AND t1.playerTime = t2.bestTime) u GROUP BY Name ORDER BY recordsCount DESC) v, (SELECT @pos := 0) i1, (SELECT @prev := -1) i2 LIMIT 10;')
 
   const [topPoints] = await connection.execute('SELECT @pos := @pos + 1 AS v1, @rank := IF(@prev = Points, @rank, @pos) AS rank, @prev := Points AS v2, Name, Points FROM race_points, (SELECT @pos := 0) i1, (SELECT @prev := -1) i2 WHERE Points > 0 ORDER BY Points DESC LIMIT 10;')
@@ -38,7 +36,6 @@ router.get('/', async function (req, res, next) {
     title: 'Ranks | Unique',
     user: req.session.authed ? req.session.user : null,
     topPoints: topPoints,
-    maxPoints: mapCount[0].numMaps * 100,
     lastTopRanks: lastTopRanks,
     formatTime: formatTime,
     mapRecords: mapRecords
