ecd3a86e8fd25a7c94c19d8b75776e6c61a0063f
Ryozuki <edgar@ryobyte.com>
2018-05-19 20:40:18 +0200

    static file serving changes and initial admin zone login

---
+++ public/js/adminlogin.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var form = new BulmaFormHandler('#form-admin', '/admin/login', 'POST')
+    form.onResponse((status, res) => {
+      switch (status) {
+        case 400:
+        case 422:
+        {
+          $('#response-message').text('Invalid password.')
+          break
+        }
+        case 201:
+        {
+            window.location.replace('/admin')
+            break
+        }
+      }
+    })
+  })
+  
