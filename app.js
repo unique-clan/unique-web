5a1d44d6071c817891ee4056f00c49cff9b66de2
Edgar <git@edgarluque.com>
2018-07-13 10:57:54 +0200

    update packages

--- app.js
+++ app.js
@@ -67,12 +67,7 @@ app.use(cookieParser())
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/static/css', express.static(path.join(__dirname, 'node_modules/bulma/css')))
-app.use('/static/css', express.static(path.join(__dirname, 'node_modules/bulma-extensions/dist')))
-var bulmaExtensions = ['tagsinput']
-for (var x in bulmaExtensions) {
-  var ext = bulmaExtensions[x]
-  app.use('/static/js', express.static(path.join(__dirname, `node_modules/bulma-extensions/bulma-${ext}/dist`)))
-}
+app.use('/static/', express.static(path.join(__dirname, 'node_modules/bulma-extensions/dist')))
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
