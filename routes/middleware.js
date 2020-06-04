67cd21f27800839ce0618e7e6396ac4ad927e38e
Edgar <git@edgarluque.com>
2020-06-04 12:01:27 +0200

    update deps and add linter fixer

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,20 +1,20 @@
 var isAuthed = function (req, res, next) {
-  if (req.session && req.session.authed) {
-    next();
-  } else {
-    res.redirect('/');
-  }
+    if (req.session && req.session.authed) {
+        next();
+    } else {
+        res.redirect("/");
+    }
 };
 
 var isNotAuthed = function (req, res, next) {
-  if (!req.session || !req.session.authed) {
-    next();
-  } else {
-    res.redirect('/');
-  }
+    if (!req.session || !req.session.authed) {
+        next();
+    } else {
+        res.redirect("/");
+    }
 };
 
 module.exports = exports = {
-  isAuthed: isAuthed,
-  isNotAuthed: isNotAuthed
+    isAuthed: isAuthed,
+    isNotAuthed: isNotAuthed,
 };
